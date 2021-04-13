ed10e962ef5c2f1c10a8853aaab3636219ae3cc8
Shatur95 <genaloner@gmail.com>
2021-04-13 23:49:17 +0300

    Implement pickup respawn

--- objects/pickups/pickup.gd
+++ objects/pickups/pickup.gd
@@ -3,10 +3,21 @@ extends Area
 
 export(GDScript) var status_effect: GDScript
 
+onready var _respawn_timer: Timer = $RespawnTimer
+onready var _position: Position3D = $Position
+
+
+func _respawn() -> void:
+	_position.visible = true
+	# warning-ignore:return_value_discarded
+	connect("body_entered", self, "_on_body_entered")
+
 
 func _on_body_entered(node: Node) -> void:
 	var actor := node as Actor
 	if actor:
-		queue_free()
+		disconnect("body_entered", self, "_on_body_entered")
+		_position.visible = false
+		_respawn_timer.start()
 		if status_effect:
 			actor.apply_status_effect(status_effect)
