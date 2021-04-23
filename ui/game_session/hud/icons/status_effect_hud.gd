2e5fa85ce2de1d142bc08da5e9877020226561ba
Shatur95 <genaloner@gmail.com>
2021-04-23 22:47:10 +0300

    Handle status effects restart

--- ui/game_session/hud/icons/status_effect_hud.gd
+++ ui/game_session/hud/icons/status_effect_hud.gd
@@ -6,6 +6,10 @@ var status_effect: StatusEffect setget set_status_effect
 
 
 func set_status_effect(effect: StatusEffect) -> void:
+	if status_effect:
+		effect.get_timer().disconnect("started", self, "_display_timer")
 	status_effect = effect
 	texture = load(Utils.get_script_icon(effect.script))
 	_display_timer(effect.get_timer().get_time_left(), true)
+	# warning-ignore:return_value_discarded
+	effect.get_timer().connect("started", self, "_display_timer", [true])
